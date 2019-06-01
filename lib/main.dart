b6ace76e08798441b425e9e23f949abc04028f4f
jhonatan torres <yon.t.f@hotmail.com>
2019-06-01 14:29:36 -0500

    create post

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fluttering/screens/home.dart';
+import 'package:fluttering/screens/create_post.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Home(),
+      home: CreatePost(),
     );
   }
 }
